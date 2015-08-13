a8bedc4db1ddd88e1f41a179d754a86becde6bcc
Jamie Bird <jamie06bird@gmail.com>
2015-08-13 14:11:10 +0100

    Dimension matching can now be done on multiple dimensions to enable matching on devices with the same name across multiple hostnames.

--- monasca/anomaly_engine/processors/rde_multi_anomaly_processor.py
+++ monasca/anomaly_engine/processors/rde_multi_anomaly_processor.py
@@ -27,7 +27,7 @@ class RDEMultiAnomalyProcessor(AnomalyProcessor):
 		self.fault_threshold 	= rde_config.fault_threshold
 		self.normal_threshold 	= rde_config.normal_threshold
 
-		# what dimension to match the samples on
+		# what dimension to match the samples on - LIST
 		self.dimension_match = rde_multi_config.dimension_match
 
 		# what to name the sample when publishing back - log file!
@@ -45,65 +45,63 @@ class RDEMultiAnomalyProcessor(AnomalyProcessor):
 		name 		= metric['name']
 		dimensions 	= metric['dimensions']
 		value 		= metric['value']
-		match_value	= dimensions[self.dimension_match]	# should check it actually exists
+		
+		# string containing the dimensions to metrics by
+		# e.g "compute1.eth0" or "compute2./home/mon-agent/commsmain-2.pcap"
+		match_str 	= '.'.join([dimensions[x] for x in self.dimension_match])	# should check dimensions actually exists
 
 		# fill buffer
-		if match_value not in self._sample_buffer:
-			self._sample_buffer[match_value] = {}
+		if match_str not in self._sample_buffer:
+			self._sample_buffer[match_str] = {}
 		
-		self._sample_buffer[match_value][name] = value;
+		self._sample_buffer[match_str][name] = value;
 
-		print("Received " + name + " for " + self.dimension_match + " "  + match_value)
+		print("Received " + name + " for " + self.dimension_match + " "  + match_str)
 
 		# if buffer is full process sample
-		if len(self._sample_buffer[match_value]) == len(self.metrics):
+		if len(self._sample_buffer[match_str]) == len(self.metrics):
 
-			print("Sample buffer full for " + match_value)
+			print("Sample buffer full for " + match_str)
 
-			sample = []
-
-			# loop through to create sample list so features are always in the same order 
-			# rather than using .values()
-			for x in self.metrics:
-				sample.append(self._sample_buffer[match_value][x])
+			#create sample
+			sample = [self._sample_buffer[match_str][x] for x in self.metrics]
 
 			#send to anomaly detection
-			anom_values = self.rde(sample, match_value)
+			anom_values = self.rde(sample, match_str)
 
-			#publish results
-
-			#remove all dimensions except the one were matching on
-			match = metric['dimensions'][self.dimension_match]
+			#remove all dimensions except the ones were matching on
+			match = {x: dimensions[x] for x in self.dimension_match}
 			metric['dimensions'].clear()
-			metric['dimensions'][self.dimension_match] = match	
-	
+			metric['dimensions'] = match 
+
+			#publish results	
 			# anomaly score
 			metric['name'] = self.sample_name + '.rde.anomaly_score'
-		       	metric['value'] = anom_values['status']
-		       	str_value = simplejson.dumps(metric_envelope)
-	       		self._producer.send_messages(self._topic, str_value)
+			metric['value'] = anom_values['status']
+			str_value = simplejson.dumps(metric_envelope)
+			self._producer.send_messages(self._topic, str_value)
 
 			# density
 			metric['name'] = self.sample_name + '.rde.density'
-                        metric['value'] = anom_values['density']
-                        str_value = simplejson.dumps(metric_envelope)
-                        self._producer.send_messages(self._topic, str_value)			
+			metric['value'] = anom_values['density']
+			str_value = simplejson.dumps(metric_envelope)
+			self._producer.send_messages(self._topic, str_value)			
 
 			# mean density
 			metric['name'] = self.sample_name + '.rde.mean_density'
-                        metric['value'] = anom_values['mean_density']
-                        str_value = simplejson.dumps(metric_envelope)
-                        self._producer.send_messages(self._topic, str_value)
+			metric['value'] = anom_values['mean_density']
+			str_value = simplejson.dumps(metric_envelope)
+			self._producer.send_messages(self._topic, str_value)
 
-	       		# clear sample buffer
-	       	 	for x in self.metrics:
-	        		del self._sample_buffer[match_value][x]
+			# clear sample buffer
+			for x in self.metrics:
+				del self._sample_buffer[match_str][x]
 
-	def rde(self, sample, match_value):
+	def rde(self, sample, match_str):
 
-		#first iteration of match_value - init anomaly values
-		if match_value not in self._anom_values:
-			self._anom_values[match_value] = {
+		#first iteration of match_str - init anomaly values
+		if match_str not in self._anom_values:
+			self._anom_values[match_str] = {
 				'mean': sample, 
 				'density': 1.0,
 				'mean_density': 1.0, 
@@ -114,12 +112,12 @@ class RDEMultiAnomalyProcessor(AnomalyProcessor):
 				'normal_flag': 0,
 				'fault_flag': 0
 			}
-			anom_values = self._anom_values[match_value]
+			anom_values = self._anom_values[match_str]
 
 		#original RDE
 		else:
 			#bring local anomaly values
-			anom_values = self._anom_values[match_value]	
+			anom_values = self._anom_values[match_str]	
 				
 			anom_values['mean'] 		= [(((anom_values['k']-1)/anom_values['k'])*anom_values['mean'][idx])+((1/anom_values['k'])*x) for idx,x in enumerate(sample)]	 
 			anom_values['scalar'] 		= (((anom_values['k']-1)/anom_values['k'])*anom_values['scalar'])+((1/anom_values['k'])*(numpy.linalg.norm(numpy.array(sample))**2))
